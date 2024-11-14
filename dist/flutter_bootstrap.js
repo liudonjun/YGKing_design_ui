01be3fbe296be2762681cfa76152cd9f31a387f3
YGJoon <liudongjun@ldjun.cn>
2024-11-14 18:00:50 +0800

    deploy

--- dist/flutter_bootstrap.js
+++ dist/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "652519140"
+    serviceWorkerVersion: "2052092609"
   }
 });
